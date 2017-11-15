dfdc4bd659676e13b316e268f70023b047926b7f
Jaroslav Hejlek <hejlekjaroslav@gmail.com>
2017-11-15 12:05:23 +0100

    Cleaned up repo

--- dist/templates/sheet-footer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.default = `
-    </sheetData>
-    <pageMargins left="0.7" right="0.7" top="0.75" bottom="0.75" header="0.3" footer="0.3"/>
-</worksheet>`;
