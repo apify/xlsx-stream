4797dca309ea1edd88f68a96bdf78b794646dff1
Jaroslav Hejlek <hejlekjaroslav@gmail.com>
2018-01-11 15:58:47 +0100

    Sanitize string - remove invalid XML chars

--- src/utils.js
+++ src/utils.js
@@ -11,8 +11,25 @@ export function getCellId(rowIndex, cellIndex) {
     return `${cellXPosition}${rowIndex + 1}`;
 }
 
-export function escape(text) {
-    return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+export function sanitize(text) {
+    const escaped = text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    const sourceBuffer = Buffer.from(escaped);
+    const buffer = Buffer.alloc(sourceBuffer.byteLength); // allocate enough space
+    let letter;
+    let writtenLength = 0;
+    for (let i = 0; i < escaped.length; i++) {
+        letter = escaped[i];
+        if (
+            (letter === '\x09') ||
+            (letter === '\x0A') ||
+            (letter === '\x0D') ||
+            ((letter >= '\x20') && (letter <= '\uD7FF')) ||
+            ((letter >= '\uE000') && (letter <= '\uFFFD'))
+        ) {
+            writtenLength += buffer.write(letter, writtenLength);
+        }
+    }
+    return buffer.toString('utf8', 0, writtenLength);
 }
 
 export function timeoutPromised(timeout) {
