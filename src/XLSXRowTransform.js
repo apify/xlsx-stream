77090a4c122231ced22677cad804a742f9d202f8
Jenny Zhu <jenny.zhu@imply.io>
2018-10-29 21:31:12 -0700

    Extract the 2D array function

--- src/XLSXRowTransform.js
+++ src/XLSXRowTransform.js
@@ -7,12 +7,17 @@ export default class XLSXRowTransform extends Transform {
         super({ objectMode: true });
         this.rowCount = 0;
     }
+
+    static is2DArray(arr) {
+        return (arr[0] !== undefined && arr[0].constructor === Array);
+    }
+
     /**
      * Transform array to row string
      */
     _transform(row, encoding, callback) { // eslint-disable-line
         let xlsxRow = '';
-        if (row[0] !== undefined && row[0].constructor === Array) { // check if 2d Array
+        if (XLSXRowTransform.is2DArray(row)) {
             for (let i = 0; i < row.length; i++) {
                 xlsxRow += Row(this.rowCount++, row[i]);
             }
