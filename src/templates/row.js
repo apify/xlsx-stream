81ff24a8ac84b37f4e57484045edee3725f501aa
Jaroslav Hejlek <hejlekjaroslav@gmail.com>
2019-09-26 13:39:44 +0200

    Fixed whitespaces in rows

--- src/templates/row.js
+++ src/templates/row.js
@@ -3,9 +3,8 @@ import cell from './cell';
 import { getCellId } from '../utils';
 
 export default function row(index, values, shouldFormat) {
-    return ` <row r="${index + 1}" spans="1:${values.length}" x14ac:dyDescent="0.2">${
-        values
-            .map((cellValue, cellIndex) => cell(cellValue, getCellId(index, cellIndex), shouldFormat))
-            .join("\n\t\t\t")
-    } </row>`;
+    return `
+    <row r="${index + 1}" spans="1:${values.length}" x14ac:dyDescent="0.2">
+        ${values.map((cellValue, cellIndex) => cell(cellValue, getCellId(index, cellIndex), shouldFormat)).join("\n\t\t\t")}
+    </row>`;
 }
