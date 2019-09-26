f861fc16ddf1d09a82d0bf2e3f3c20d1aae31ddb
Jaroslav Hejlek <hejlekjaroslav@gmail.com>
2019-09-26 13:53:17 +0200

    One last change to whitespace structure

--- src/templates/row.js
+++ src/templates/row.js
@@ -4,7 +4,7 @@ import { getCellId } from '../utils';
 
 export default function row(index, values, shouldFormat) {
     return `
-    <row r="${index + 1}" spans="1:${values.length}" x14ac:dyDescent="0.2">
-        ${values.map((cellValue, cellIndex) => cell(cellValue, getCellId(index, cellIndex), shouldFormat)).join("\n\t\t\t")}
-    </row>`;
+        <row r="${index + 1}" spans="1:${values.length}" x14ac:dyDescent="0.2">
+            ${values.map((cellValue, cellIndex) => cell(cellValue, getCellId(index, cellIndex), shouldFormat)).join("\n\t\t\t")}
+        </row>`;
 }
