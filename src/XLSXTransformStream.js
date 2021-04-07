3d4ded8b4e3f7d076e5321e8bee469f06677a91c
Jiri Moravcik <jiri.moravcik@gmail.com>
2021-04-07 14:38:59 +0200

    feat: Create XLSXTransformStream with consistent API

---
+++ src/XLSXTransformStream.js
@@ -0,0 +1,75 @@
+import Archiver from 'archiver';
+import { PassThrough, Transform } from 'stream';
+import XLSXRowTransform from './XLSXRowTransform';
+import * as templates from './templates';
+
+/** Class representing a XLSX Transform Stream */
+export default class XLSXTransformStream extends Transform {
+    /**
+     * Create a new Stream
+     */
+    constructor() {
+        super({ objectMode: true });
+        this.initializeArchiver();
+        this.rowTransform = new XLSXRowTransform();
+
+        this.sheetStream = new PassThrough();
+        this.sheetStream.write(templates.SheetHeader);
+        this.rowTransform.pipe(this.sheetStream);
+        this.zip.append(this.sheetStream, {
+            name: 'xl/worksheets/sheet1.xml',
+        });
+    }
+
+    initializeArchiver() {
+        this.zip = Archiver('zip', {
+            forceUTC: true,
+        });
+
+        this.zip.on('data', (data) => {
+            this.push(data);
+            console.log('zip data', data);
+        });
+
+        this.zip.catchEarlyExitAttached = true;
+
+        this.zip.append(templates.ContentTypes, {
+            name: '[Content_Types].xml',
+        });
+
+        this.zip.append(templates.Rels, {
+            name: '_rels/.rels',
+        });
+
+        this.zip.append(templates.Workbook, {
+            name: 'xl/workbook.xml',
+        });
+
+        this.zip.append(templates.Styles, {
+            name: 'xl/styles.xml',
+        });
+
+        this.zip.append(templates.WorkbookRels, {
+            name: 'xl/_rels/workbook.xml.rels',
+        });
+
+        this.zip.on('warning', (err) => {
+            console.warn(err);
+        });
+
+        this.zip.on('error', (err) => {
+            console.error(err);
+        });
+    }
+
+    _transform(row, encoding, callback) {
+        this.rowTransform.write(row);
+        callback();
+    }
+
+    async _flush(callback) {
+        this.sheetStream.end(templates.SheetFooter);
+        await this.zip.finalize();
+        callback();
+    }
+}
