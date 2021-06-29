fb28be6d674e1871064efbe6a536ff3209f62341
Jiri Moravcik <jiri.moravcik@gmail.com>
2021-06-30 00:45:49 +0200

    refactor(cell): use snake case for constants

--- src/templates/cell.js
+++ src/templates/cell.js
@@ -2,15 +2,15 @@ import { isDate, isString, isNumber, isBoolean } from 'lodash';
 import { sanitize } from '../utils';
 
 // 25569 = Days between 1970/01/01 and 1900/01/01 (min date in Windows Excel)
-const offset = 25569;
+const OFFSET_DAYS = 25569;
 
 // 24 * 60 * 60 * 1000
-const millisecondsInOneDay = 86400000;
+const MILLISECONDS_IN_ONE_DAY = 86400000;
 
 export default function (value, cell) {
     if (isDate(value)) {
         const unixTimestamp = value.getTime();
-        const officeTimestamp = (unixTimestamp / millisecondsInOneDay) + offset;
+        const officeTimestamp = (unixTimestamp / MILLISECONDS_IN_ONE_DAY) + OFFSET_DAYS;
         return `<c r="${cell}" t="n"><v>${officeTimestamp}</v></c>`;
     } else if (isString(value)) {
         return `<c r="${cell}" t="inlineStr"><is><t>${sanitize(value)}</t></is></c>`;
