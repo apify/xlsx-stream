4391d552af11a7113423d13e71e32e726049ecd3
Brett Willis <brettjohnwillis@gmail.com>
2024-05-11 21:59:06 +1200

    Remove lodash dependency

--- src/templates/cell.js
+++ src/templates/cell.js
@@ -1,4 +1,3 @@
-import { isDate, isString, isNumber, isBoolean } from 'lodash';
 import { sanitize, getNumberFormat, getDateFormat } from '../utils';
 
 // 25569 = Days between 1970/01/01 and 1900/01/01 (min date in Windows Excel)
@@ -8,16 +7,16 @@ const OFFSET_DAYS = 25569;
 const MILLISECONDS_IN_ONE_DAY = 86400000;
 
 export default function (value, cell, shouldFormat) {
-    if (isDate(value)) {
+    if (value instanceof Date) {
         const unixTimestamp = value.getTime();
         const officeTimestamp = (unixTimestamp / MILLISECONDS_IN_ONE_DAY) + OFFSET_DAYS;
         const maybeFormat = shouldFormat && getDateFormat();
         return `<c r="${cell}" t="n"${maybeFormat ? ` s="${maybeFormat}"` : ''}><v>${officeTimestamp}</v></c>`;
-    } else if (isString(value)) {
+    } else if (typeof value === 'string') {
         return `<c r="${cell}" t="inlineStr"><is><t>${sanitize(value)}</t></is></c>`;
-    } else if (isBoolean(value)) {
+    } else if (typeof value === 'boolean') {
         return `<c r="${cell}" t="inlineStr"><is><t>${value}</t></is></c>`;
-    } else if (isNumber(value)) {
+    } else if (typeof value === 'number') {
         const maybeFormat = shouldFormat && getNumberFormat(value);
         return `<c r="${cell}" t="n"${maybeFormat ? ` s="${maybeFormat}"` : ''}><v>${value}</v></c>`;
     } else if (value) {
