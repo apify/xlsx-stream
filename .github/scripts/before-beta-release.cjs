d96634cf4e44743517dc964ad1d4d695b4c6383f
František Nesveda <fnesveda@users.noreply.github.com>
2026-05-07 12:09:54 +0200

    chore: Update NPM publishing workflow to use OIDC

--- .github/scripts/before-beta-release.cjs
+++ .github/scripts/before-beta-release.cjs
@@ -1,32 +1,35 @@
-const path = require('path');
-const fs = require('fs');
-const { execSync } = require('child_process');
+const { execSync } = require('node:child_process');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const PKG_JSON_PATH = path.join(__dirname, '..', '..', 'package.json');
 
+// eslint-disable-next-line import/no-dynamic-require
 const pkgJson = require(PKG_JSON_PATH);
 
 const PACKAGE_NAME = pkgJson.name;
 const VERSION = pkgJson.version;
 
 const nextVersion = getNextVersion(VERSION);
+// eslint-disable-next-line no-console
 console.log(`before-deploy: Setting version to ${nextVersion}`);
 pkgJson.version = nextVersion;
 
-fs.writeFileSync(PKG_JSON_PATH, JSON.stringify(pkgJson, null, 2) + '\n');
+fs.writeFileSync(PKG_JSON_PATH, `${JSON.stringify(pkgJson, null, 2)}\n`);
 
 function getNextVersion(version) {
-    const versionString = execSync(`npm show ${PACKAGE_NAME} versions --json`, { encoding: 'utf8'});
+    const versionString = execSync(`npm show ${PACKAGE_NAME} versions --json`, { encoding: 'utf8' });
     const versions = JSON.parse(versionString);
 
-    if (versions.some(v => v === VERSION)) {
+    if (versions.some((v) => v === VERSION)) {
+        // eslint-disable-next-line no-console
         console.error(`before-deploy: A release with version ${VERSION} already exists. Please increment version accordingly.`);
         process.exit(1);
     }
 
     const prereleaseNumbers = versions
-        .filter(v => (v.startsWith(VERSION) && v.includes('-')))
-        .map(v => Number(v.match(/\.(\d+)$/)[1]));
+        .filter((v) => (v.startsWith(VERSION) && v.includes('-')))
+        .map((v) => Number(v.match(/\.(\d+)$/)[1]));
     const lastPrereleaseNumber = Math.max(-1, ...prereleaseNumbers);
-    return `${version}-beta.${lastPrereleaseNumber + 1}`
+    return `${version}-beta.${lastPrereleaseNumber + 1}`;
 }
